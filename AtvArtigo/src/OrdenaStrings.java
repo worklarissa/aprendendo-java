0c40dcb2339e3e4195951964fbc8fc7e48948c69
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-03-09 23:39:33 -0300

    Assisti mais um pouco do video

--- AtvArtigo/src/OrdenaStrings.java
+++ AtvArtigo/src/OrdenaStrings.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
 		
 		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+		
+		Comparator
+		Collections.sort(palavras);
+		System.out.println(palavras);
+		
+		
 	}
+}
+
+class ComparadorPorTamanho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length()< s2.length())
+			return 1;
+		return 0;
+	}
+	
+	
 }
